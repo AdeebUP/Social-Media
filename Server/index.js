788d6c75e2ca261a861bba03c17be3caa89cfa60
Adeeb Ulpara <upadeeb@gmail.com>
2022-11-27 01:00:50 +0530

    models and routes are added

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import AutoRoute from './Routes/AuthRoute.js'
+
+// Routes
 
 const app = express();
+
+// Middleware
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
@@ -20,3 +26,7 @@ mongoose
     .then(() => app.listen(process.env.PORT, () =>
         console.log(`Listening at ${process.env.PORT}`)))
     .catch((error) => console.log(error))
+
+    // usage fo routes
+
+    app.use('/auth', AutoRoute)

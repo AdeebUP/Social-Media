55cc87317886c90159ac9bd424f000f61533aac8
Adeeb Ulpara <upadeeb@gmail.com>
2022-11-29 16:15:53 +0530

    User Controller follow

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -30,10 +30,11 @@ export const upadateUser = async (req, res) => {
     if (id === currentUserId || currentUserAdminStatus) {
         try {
 
-            if (password) {
-                const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrypt.hash(password, salt)
-            }
+            // if (password) {
+            //     const salt = await bcrypt.genSalt(10);
+            //     req.body.password = await bcrypt.hash(password, salt)
+            // }
+            console.log(req.body)
 
             const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true })
             res.status(200).json(user)
@@ -65,3 +66,59 @@ export const deleteUser = async (req, res) => {
 }
 
 // Follow a User
+
+export const followUser = async (req, res) => {
+    const id = req.params.id
+
+    const {currentUserId} = req.body
+
+    if (currentUserId === id) {
+        res.status(403).json("Action forbidden")
+    }
+    else{
+        try {
+            const followUser = await UserModel.findById(id)
+            const followingUser = await UserModel.findById(currentUserId)
+
+            if(!followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({ $push : {followers: currentUserId}})
+                await followingUser.updateOne({ $push : {following : id}})
+                res.status(200).json("User followed!")
+            }
+            else{
+                res.status(403).json("User is Already followed by you")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
+
+// UnFollow a user
+
+export const UnFollowUser = async (req, res) => {
+    const id = req.params.id
+
+    const {currentUserId} = req.body
+
+    if (currentUserId === id) {
+        res.status(403).json("Action forbidden")
+    }
+    else{
+        try {
+            const followUser = await UserModel.findById(id)
+            const followingUser = await UserModel.findById(currentUserId)
+
+            if(followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({ $pull : {followers: currentUserId}})
+                await followingUser.updateOne({ $pull : {following : id}})
+                res.status(200).json("User Unfollowed!")
+            }
+            else{
+                res.status(403).json("User is not followed by you")
+            }
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
--- Server/Routes/UserRoute.js
+++ Server/Routes/UserRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { deleteUser, getUser, upadateUser } from "../Controllers/UserController.js";
+import { deleteUser, followUser, getUser, UnFollowUser, upadateUser } from "../Controllers/UserController.js";
 
 const router = express.Router();
 
 router.get('/:id', getUser)
 router.put('/:id', upadateUser)
 router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', UnFollowUser)
 
 export default router;

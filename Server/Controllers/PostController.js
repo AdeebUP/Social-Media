bcfa03859595552eaac1b556f1c4f49004d42480
Adeeb Ulpara <upadeeb@gmail.com>
2022-11-30 00:05:24 +0530

    server side

--- Server/Controllers/PostController.js
+++ Server/Controllers/PostController.js
@@ -1,10 +1,11 @@
 import PostModel from "../Models/postModel.js";
 import mongoose from "mongoose";
+import UserModel from "../Models/userModel.js";
 
 
 // Create new Post 
 
-export const createPost = async(req, res) => {
+export const createPost = async (req, res) => {
     const newPost = new PostModel(req.body)
 
     try {
@@ -17,7 +18,7 @@ export const createPost = async(req, res) => {
 
 // Get a post
 
-export const getPost = async(req, res) => {
+export const getPost = async (req, res) => {
     const id = req.params.id
     try {
         const post = await PostModel.findById(id)
@@ -29,17 +30,17 @@ export const getPost = async(req, res) => {
 
 // Update a post
 
-export const updatePost = async(req, res) => {
+export const updatePost = async (req, res) => {
     const postId = req.params.id
-    const {userId} = req.body
-    
+    const { userId } = req.body
+
     try {
         const post = await PostModel.findById(postId)
-        if(post.userId === userId){
-            await post.updateOne({ $set : req.body })
+        if (post.userId === userId) {
+            await post.updateOne({ $set: req.body })
             res.status(200).json("Post Updated")
         }
-        else{
+        else {
             res.status(403).json("Action forbidden")
         }
     } catch (error) {
@@ -51,14 +52,14 @@ export const updatePost = async(req, res) => {
 
 export const deletePost = async (req, res) => {
     const id = req.params.id;
-    const {userId} = req.body
+    const { userId } = req.body
     try {
         const post = await PostModel.findById(id)
-        if(post.userId === userId){
+        if (post.userId === userId) {
             await post.deleteOne();
             res.status(200).json("Post deleted successfully")
         }
-        else{
+        else {
             res.status(403).json("Action forbidden")
         }
     } catch (error) {
@@ -70,17 +71,55 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const id = req.params.id
-    const {userId} = req.body
+    const { userId } = req.body
     try {
         const post = await PostModel.findById(id)
-        if(!post.likes.includes(userId)){
-            await post.updateOne({ $push : {likes : userId}})
+        if (!post.likes.includes(userId)) {
+            await post.updateOne({ $push: { likes: userId } })
             res.status(200).json("Post liked")
         }
-        else{
-            await post.updateOne({ $pull : {likes : userId}})
+        else {
+            await post.updateOne({ $pull: { likes: userId } })
             res.status(200).json("Post Unliked")
         }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// Get Timeline Posts
+
+export const getTimelinePosts = async (req, res) => {
+    const userId = req.params.id
+    try {
+        const currentUserPosts = await PostModel.find({ userId: userId })
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "posts",
+                    localField: "following",
+                    foreignField: "userId",
+                    as: "followingPosts"
+                }
+            },
+            {
+                $project: {
+                    followingPosts: 1,
+                    _id: 0
+                }
+            }
+        ])
+        res.status(200)
+            .json(currentUserPosts.concat(...followingPosts[0].followingPosts)
+                .sort((a, b) => {
+                    return b.createdAt - a.createdAt;
+                })
+            )
     } catch (error) {
         res.status(500).json(error)
     }
--- Server/Routes/PostRoute.js
+++ Server/Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, deletePost, getPost, likePost, updatePost } from "../Controllers/PostController.js";
+import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from "../Controllers/PostController.js";
 const router = express.Router()
 
 router.post('/', createPost)
@@ -7,5 +7,6 @@ router.get('/:id', getPost)
 router.put('/:id', updatePost)
 router.delete('/:id', deletePost)
 router.put('/:id/like', likePost)
+router.get('/:id/timeLine', getTimelinePosts)
 
 export default router;

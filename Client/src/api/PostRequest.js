79fe94ee0a1f100363e37ccf84ac98d21f0e5fef
Adeeb Ulpara <upadeeb@gmail.com>
2022-12-05 16:18:18 +0530

    like post

--- Client/src/api/PostRequest.js
+++ Client/src/api/PostRequest.js
@@ -3,3 +3,5 @@ import axios from "axios";
 const API = axios.create({baseURL: "http://localhost:5000"})
 
 export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`)
+
+export const likePost = (id, userId) => API.put(`post/${id}/like`, {userId: userId})

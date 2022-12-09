fb98d7fc7d55c2196d8c275a803cc600b5f64abc
Adeeb Ulpara <upadeeb@gmail.com>
2022-12-10 02:12:40 +0530

    jwt middleware

--- Client/src/api/UserRequest.js
+++ Client/src/api/UserRequest.js
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" })
 
+API.interceptors.request.use((req) =>{
+    if(localStorage.getItem('profile')){
+        req.headers.Authorization = `Beaner ${JSON.stringify(localStorage.getItem('profile').token)}`
+    }
+    return req
+})
+
 export const getUser = (userId) => API.get(`/user/${userId}`)
 
 export const updateUser = (id, formData) => API.put(`/user/${id}`, formData)
--- Server/Routes/UserRoute.js
+++ Server/Routes/UserRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { deleteUser, followUser, getUser, UnFollowUser, upadateUser, getAllUsers } from "../Controllers/UserController.js";
+import authMiddleWare from "../MiddleWare/authMiddleWare.js";
 
 const router = express.Router();
 
 router.get('/', getAllUsers)
 router.get('/:id', getUser)
-router.put('/:id', upadateUser)
-router.delete('/:id', deleteUser)
-router.put('/:id/follow', followUser)
-router.put('/:id/unfollow', UnFollowUser)
+router.put('/:id',authMiddleWare, upadateUser)
+router.delete('/:id',authMiddleWare, deleteUser)
+router.put('/:id/follow',authMiddleWare, followUser)
+router.put('/:id/unfollow',authMiddleWare, UnFollowUser)
 
 export default router;

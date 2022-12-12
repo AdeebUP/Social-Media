d217ef3537cdc32576ca44422d37b17a79e07d72
Adeeb Ulpara <upadeeb@gmail.com>
2022-12-12 13:17:43 +0530

    REST api

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,7 @@ import AutoRoute from './Routes/AuthRoute.js'
 import UserRoute from "./Routes/UserRoute.js"
 import PostRoute from "./Routes/PostRoute.js"
 import UploadRoute from './Routes/UploadRoute.js'
+import MessageRoute from './Routes/MessageRoute.js'
 
 import ChatRoute from './Routes/ChatRoute.js'
 // Routes
@@ -44,3 +45,4 @@ app.use('/user', UserRoute)
 app.use('/post', PostRoute)
 app.use('/upload', UploadRoute)
 app.use('/chat', ChatRoute)
+app.use('/message', MessageRoute)
